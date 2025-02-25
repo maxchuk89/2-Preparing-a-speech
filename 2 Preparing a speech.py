ebf5498dcdd0ac544a8b1bb2bb08f26415ad9474
maxchuk89 <maxchuk89@gmail.com>
2025-02-25 19:26:19 +0300

    Убрал лишнюю функцию 2 Preparing a speech.py

--- 2 Preparing a speech.py
+++ 2 Preparing a speech.py
@@ -1,5 +1,5 @@
 
-from transliterate import translit, get_available_language_codes
+from transliterate import translit
 from num2words import num2words
 
 print(translit("Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.", 'ru'))
